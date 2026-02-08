683198d6cee56e1316397bd424c2769d7a806ee4
Pedro Pinto <phgoncalves2603@gmail.com>
2026-02-08 16:32:31 -0500

    refactor: rename main  to lexerTest

--- src/lexer/lexerTest.java
+++ src/lexer/lexerTest.java
@@ -3,7 +3,7 @@ package lexer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Main {
+public class lexerTest {
     public static void main(String[] args) throws Exception {
         String code = Files.readString(Path.of("input.txt"));
 
