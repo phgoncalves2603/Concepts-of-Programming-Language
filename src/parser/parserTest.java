4456d0c5f15af4230629ab07418a67819d74f873
phgoncalves2603 <phgoncalves2603@gmail.com>
2026-03-26 22:28:28 -0400

    fix: correcting a typo

--- src/parser/parserTest.java
+++ src/parser/parserTest.java
@@ -8,14 +8,14 @@ public class parserTest {
 
     public static void main(String[] args) throws Exception {
 
-//        if (args.length == 0) {
-//            System.out.println("Error: No input file provided.\n");
-//            System.out.println("Usage: java parser.parserTest <inputfile>");
-//            System.out.println("Example: java parser.parserTest input.txt");
-//            return;
-//        }
+        if (args.length == 0) {
+            System.out.println("Error: No input file provided.\n");
+            System.out.println("Usage: java parser.parserTest <inputfile>");
+            System.out.println("Example: java parser.parserTest input.txt");
+            return;
+        }
 
-        String code = Files.readString(Path.of("/Users/phgoncalves/Downloads/Concepts-of-Programming-Language/src/input.txt"));
+        String code = Files.readString(Path.of(args[0]));
 
         LexicalAnalyzer lexer = new LexicalAnalyzer(code);
 
